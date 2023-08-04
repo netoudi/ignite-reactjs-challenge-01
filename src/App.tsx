61b771d8b618d0ba532f2fb0656964581cf008d7
netoudi <netoudi@rocketmail.com>
2023-08-04 08:00:00 -0300

    feat: implements todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,83 @@
+import React from 'react';
 import logo from '@app/assets/logo.svg';
 import styles from './App.module.css';
 
+interface Task {
+  id: string;
+  description: string;
+  completed: boolean;
+}
+
+function createTask(value: string): Task {
+  return {
+    id: Date.now().toString(),
+    description: value,
+    completed: false,
+  };
+}
+
 export function App() {
+  const [tasks, setTasks] = React.useState<Task[]>([]);
+  const [task, setTask] = React.useState<string>('');
+
+  const totalTasks = tasks.length;
+  const completedTasks = tasks.filter((task) => task.completed).length;
+
+  function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
+    setTask(e.target.value);
+  }
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    setTasks((state) => [createTask(task), ...state]);
+    setTask('');
+  }
+
+  function completeTask(taskId: string) {
+    setTasks((state) => {
+      return state.map((task) => {
+        if (task.id !== taskId) {
+          return task;
+        }
+        return {
+          ...task,
+          completed: !task.completed,
+        };
+      });
+    });
+  }
+
+  function deleteTask(taskId: string) {
+    setTasks((state) => state.filter((task) => task.id !== taskId));
+  }
+
   return (
     <>
       <header className={styles.header}>
         <img src={logo} alt="Todo logo" />
       </header>
       <main className={styles.main}>
-        <form className={styles.todoForm}>
-          <input type="text" placeholder="Adicione uma nova tarefa" />
-          <button>+</button>
+        <form onSubmit={handleSubmit} className={styles.todoForm}>
+          <input type="text" placeholder="Adicione uma nova tarefa" value={task} onChange={handleInput} />
+          <button type="submit">+</button>
         </form>
         <div className={styles.todoHeader}>
           <div>
-            Tarefas criadas <span>0</span>
+            Tarefas criadas <span>{totalTasks}</span>
           </div>
           <div>
-            Concluídas <span>0</span>
+            Concluídas
+            <span>{completedTasks === 0 && totalTasks === 0 ? '0' : `${completedTasks} de ${totalTasks}`}</span>
           </div>
         </div>
         <div className={styles.todoList}>
-          {[1, 2, 3, 4, 5].map((value) => (
-            <div className={styles.todoItem} key={value}>
+          {tasks.map((item) => (
+            <div className={styles.todoItem} key={item.id}>
               <div className={styles.todoControl}>
-                <input id={String(value)} type="checkbox" />
-                <label htmlFor={String(value)}>Lorem ipsum dolor sit amet.</label>
+                <input id={item.id} type="checkbox" onClick={() => completeTask(item.id)} />
+                <label htmlFor={item.id}>{item.description}</label>
               </div>
-              <button>-</button>
+              <button onClick={() => deleteTask(item.id)}>-</button>
             </div>
           ))}
         </div>
